6d258756744dfd538cc2ac44450423489c27c949
crisfreak <cristopher.sotog@utem.cl>
2021-10-22 17:30:29 -0300

    Move all de UI functions to interfaz.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-
-// Selectores
-const formulario = document.querySelector('#formulario');
-const selectorMoneda = document.querySelector('#moneda');
-const selectorCriptomoneda = document.querySelector('#criptomonedas');
-const resultado = document.querySelector('#resultado');
+import * as UI from './interfaz.js'
 
 // Código del instructor
 // const objBusqueda = {
@@ -27,16 +22,6 @@ window.onload = () => {
 //     console.log(objBusqueda);
 // }
 
-function llenarSelectorCriptomonedas(Data){
-    Data.forEach( (infoCriptomoneda) => {
-        const { CoinInfo: { FullName, Name } } = infoCriptomoneda;
-        const option = document.createElement('option');
-        option.value = Name;
-        option.textContent = FullName;
-
-        selectorCriptomoneda.appendChild(option);
-    });
-}
 
 function consultarTopTenCriptomonedas(){
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
@@ -50,52 +35,39 @@ function consultarTopTenCriptomonedas(){
 
             if( datos.Message === 'Success'){
                 const { Data } = datos;
-                llenarSelectorCriptomonedas(Data);
+                UI.llenarSelectorCriptomonedas(Data);
             }
         })
 }
 
-function mostarAlerta( mensaje, tipo, tiempo){
-    if(!document.querySelector('.alerta-personalizada')){
-
-        // Crea un contenedor
-        const alerta = document.createElement('div');
-        
-        // Dependiendo el tipo, se agregan ciertas clases
-        if( tipo === 'error'){
-            alerta.classList.add('error', 'alerta-personalizada');
-            // Agrega contenido al contenedor
-            alerta.textContent = mensaje;
-        }
-        
-        // Agrega la alerta al HTML
-        resultado.appendChild(alerta);
-        
-        // Elimina la alerta después de n segundos
-        setTimeout(() => {
-            alerta.remove();
-        }, tiempo);
-    }
-}
 
 function cotizarCriptomoneda( moneda, criptomoneda){
 
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+
+    // Muestra un spinner de carga
+    UI.mostrarSpinner();
+
+    // Hace la petición a la API
     fetch(url)
         .then( respuesta => respuesta.json())
-        .then( fullData => { console.log(fullData)})
+        .then( fullData => { UI.mostrarCotizacionHTML(fullData.DISPLAY[criptomoneda][moneda])})
 }
 
 
 function validarFormulario(e){
     e.preventDefault();
-    const moneda = selectorMoneda.value;
-    const criptomoneda = selectorCriptomoneda.value;
 
+    // Selecciona los valores
+    const moneda = UI.selectorMoneda.value;
+    const criptomoneda = UI.selectorCriptomoneda.value;
+
+    // Validan que no vengan vacíos
     if( moneda === '' || criptomoneda === ''){
-        mostarAlerta('Ambos campos son obligatorios', 'error', 2500);
+        UI.mostarAlerta('Ambos campos son obligatorios', 'error', 2500);
         return;
     }
 
+    // Hace la cotización
     cotizarCriptomoneda( moneda, criptomoneda);
 }
