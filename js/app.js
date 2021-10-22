1f8f678f033478e950b0b994803f99db616953a1
crisfreak <cristopher.sotog@utem.cl>
2021-10-22 13:22:50 -0300

    Add validation to the form and first request to the API

--- js/app.js
+++ js/app.js
@@ -5,10 +5,27 @@ const selectorMoneda = document.querySelector('#moneda');
 const selectorCriptomoneda = document.querySelector('#criptomonedas');
 const resultado = document.querySelector('#resultado');
 
+// Código del instructor
+// const objBusqueda = {
+//     moneda: '',
+//     criptomoneda: ''
+// };
+
+
 window.onload = () => {
     consultarTopTenCriptomonedas();
-    // formulario.addEventListener('submit', validarFormulario);
+    formulario.addEventListener('submit', validarFormulario);
+
+    // Código del instructor
+    // selectorMoneda.addEventListener('change', leerValorSelector);
+    // selectorCriptomoneda.addEventListener('change', leerValorSelector);
 }
+
+// Código del instructor
+// function leerValorSelector(e){
+//     objBusqueda[e.target.name] = e.target.value;
+//     console.log(objBusqueda);
+// }
 
 function llenarSelectorCriptomonedas(Data){
     Data.forEach( (infoCriptomoneda) => {
@@ -33,12 +50,33 @@ function consultarTopTenCriptomonedas(){
 
             if( datos.Message === 'Success'){
                 const { Data } = datos;
-                console.log(Data);
                 llenarSelectorCriptomonedas(Data);
             }
         })
 }
 
-// function validarFormulario(){
+// function mostarAlerta(){
 
 // }
+
+function cotizarCriptomoneda( moneda, criptomoneda){
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( fullData => { console.log(fullData)})
+}
+
+
+function validarFormulario(e){
+    e.preventDefault();
+    const moneda = selectorMoneda.value;
+    const criptomoneda = selectorCriptomoneda.value;
+
+    if( moneda === '' || criptomoneda === ''){
+        // mostarAlerta('Ambos campos son obligatorios', 'tipo', 2500);
+        return;
+    }
+
+    cotizarCriptomoneda( moneda, criptomoneda);
+}
